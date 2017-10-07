f6fabe72179d7a9bb4376d47748ab51bb3de4174
gabriel barrett <gbarrett2@gmail.com>
2017-10-07 17:14:26 -0600

    add location view model

---
+++ Closer3/Models/LocationViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Closer3.Models
+{
+    public class LocationViewModel
+    {
+        [Key]
+        public int LocationId { get; set; }
+
+        [Required]
+        [Display(Name = "Location")]
+        public string LocationName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Phone")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
